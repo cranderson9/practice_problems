d48f0072a9d198723ed11994fae86eaa0bf11b77
cale <caleanderson9@gmail.com>
2020-02-26 13:34:05 -0600

    finish problem 6

---
+++ functions_practice.rb
@@ -0,0 +1,55 @@
+# 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
+
+# def multiply_by2(number)
+#   return number * 2
+# end
+
+# p multiply_by2(4)
+
+# 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
+
+# def all_caps(string)
+#   return string.upcase
+# end
+
+# p all_caps("baseball")
+
+# 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
+
+# def subtraction(number1, number2)
+#   return number1 - number2
+# end
+
+# p subtraction(8, 3)
+
+# 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
+
+# def square(number)
+#   return number * number
+# end
+
+# p square(12)
+
+# 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
+
+# def first_letter(string)
+#   return string[0]
+# end
+
+# p first_letter("baseball")
+
+# 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
+
+def combine(string1, string2, string3)
+  return "#{string1} #{string2} #{string3}"
+end
+
+p combine("Baseball", "is", "awesome")
+
+# 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
+
+# 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
+
+# 9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
+
+# 10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
