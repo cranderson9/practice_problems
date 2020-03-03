0bc18dd66549225f8bc845cfdf62992d6e8fab9f
cale <caleanderson9@gmail.com>
2020-03-03 10:50:06 -0600

    done with #4

--- convert_data.rb
+++ convert_data.rb
@@ -14,29 +14,92 @@
 
 # p pairs_hash
 
-pairs = [[1, 3], [8, 9], [2, 16]]
-pairs_hash = {}
+# pairs = [[1, 3], [8, 9], [2, 16]]
+# pairs_hash = {}
 
-pairs.each do |pair|
-  key = pair[0]
-  value = pair[1]
-  pairs_hash[key] = value
-end
+# pairs.each do |pair|
+#   key = pair[0]
+#   value = pair[1]
+#   pairs_hash[key] = value
+# end
 
-p pairs_hash
+# p pairs_hash
 
 #  2. Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
 #     For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
 
+# items = [{ id: 1, color: "blue", price: 32 }, { id: 2, color: "red", price: 12 }]
+# id_hash = {}
+# i = 0
+
+# while i < items.length
+#   id_hash[items[i][:id]] = items[i]
+#   i += 1
+# end
+
+# p id_hash
+
+# items = [{ id: 1, color: "blue", price: 32 }, { id: 2, color: "red", price: 12 }]
+# id_hash = {}
+
+# items.each do |item|
+#   key = item[:id]
+#   value = item
+#   id_hash[key] = item
+# end
+
+# p id_hash
+
 #  3. Convert a string into a hash with keys for each letter in the string and values for the number of times the letter appears in the string.
 #     For example, "bookkeeper" becomes {"b" => 1, "o" => 2, "k" => 2, "e" => 3, "p" => 1, "r" => 1}.
+
+# word = "bookkeeper"
+# letter_count = {}
+# i = 0
+
+# while i < word.length
+#   letter = word[i]
+#   if letter_count[letter] == nil
+#     letter_count[letter] = 0
+#   end
+#   letter_count[letter] += 1
+#   i += 1
+# end
+
+# p letter_count
 
 #  4. Convert a hash into an array of arrays.
 #     For example, {"chair" => 100, "book" => 14} becomes [["chair", 100], ["book", 14]].
 
+# household_items = { "chair" => 100, "book" => 14 }
+# new_array = []
+
+# household_items.each do |name, price|
+#   new_array << [name, price]
+# end
+
+# p new_array
+
 #  5. Convert a hash into an array of hashes using the keys from each hash as the :id key in each of the array's hashes.
 #     For example, {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}} becomes [{id: 321, name: "Alice", age: 31}, {id: 322, name: "Maria", age: 27}].
 
+# people = { 321 => { name: "Alice", age: 31 }, 322 => { name: "Maria", age: 27 } }
+# people_array = []
+
+# people.each do |id, person|
+#   person[:id] = id
+#   people_array << person
+# end
+
+# p people_array
+
+people = { 321 => { name: "Alice", age: 31 }, 322 => { name: "Maria", age: 27 } }
+people_array = []
+people.each do |id, person|
+  person[:id] = id
+  people_array << person
+end
+p people_array
 #  6. Convert an array of strings into a hash with keys for each string in the array and values for the number of times the string appears in the array.
 #     For example, ["do", "or", "do", "not"] becomes {"do" => 2, "or" => 1, "not" => 1}.
 
---
+++ loop_within_loop.rb
@@ -0,0 +1,98 @@
+#  1. Use a nested loop to convert an array of number pairs into a single flattened array.
+#     For example, [[1, 3], [8, 9], [2, 16]] becomes [1, 3, 8, 9, 2, 16].
+# number_pairs = [[1, 3], [8, 9], [2, 16]]
+# flattened_numbers = []
+
+# i1 = 0
+# while i1 < number_pairs.length
+#   number_pair = number_pairs[i1]
+#   i2 = 0
+#   while i2 < number_pair.length
+#     number = number_pair[i2]
+#     flattened_numbers << number
+#     i2 += 1
+#   end
+#   i1 += 1
+# end
+# p flattened_numbers
+
+#  2. Use a nested loop with two arrays of strings to create a new array of strings with each string combined.
+#     For example, ["a", "b", "c"] and ["d", "e", "f", "g"] becomes ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"].
+
+# array1 = ["a", "b", "c"]
+# array2 = ["d", "e", "f", "g"]
+# combined_strings = []
+# i1 = 0
+# while i1 < array1.length
+#   i2 = 0
+#   while i2 < array2.length
+#     combined_strings << array1[i1] + array2[i2]
+#     i2 += 1
+#   end
+#   i1 += 1
+# end
+
+# p combined_strings
+
+#  3. Use a nested loop with one array of strings to create a new array that contains every combination of each string with every other string in the array.
+#     For example, ["a", "b", "c", "d"] becomes ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"].
+
+# letters = ["a", "b", "c", "d"]
+# combined_letters = []
+# i1 = 0
+
+# while i1 < letters.length
+#   i2 = 0
+#   while i2 < letters.length
+#     if i1 != i2
+#       combined_letters << letters[i1] + letters[i2]
+#     end
+#     i2 += 1
+#   end
+#   i1 += 1
+# end
+
+# p combined_letters
+
+#  4. Use a nested loop to find the largest product of any two different numbers within a given array.
+#     For example, [5, -2, 1, -9, -7, 2, 6] becomes 63.
+
+numbers = [5, -2, 1, -9, -7, 2, 6]
+largest_product = numbers[0] * numbers[1]
+i1 = 0
+
+while i1 < numbers.length
+  current_number = numbers[i1]
+  i2 = 0
+  while i2 < numbers.length
+    if i1 != i2
+      second_number = numbers[i2]
+      product = current_number * second_number
+      if product > largest_product
+        largest_product = product
+      end
+    end
+    i2 += 1
+  end
+  i1 += 1
+end
+
+p largest_product
+
+#  5. Use a nested loop to compute the sum of all the numbers in an array of number pairs.
+#     For example, [[1, 3], [8, 9], [2, 16]] becomes 39.
+
+#  6. Use a nested loop with two arrays of numbers to create a new array of the sums of each combination of numbers.
+#     For example, [1, 2] and [6, 7, 8] becomes [7, 8, 9, 8, 9, 10].
+
+#  7. Use a nested loop with an array of numbers to compute an array with every combination of products from each number.
+#     For example, [2, 8, 3] becomes [4, 16, 6, 16, 64, 24, 6, 24, 9].
+
+#  8. Use a nested loop to find the largest sum of any two different numbers within an array.
+#     For example, [1, 8, 3, 10] becomes 18.
+
+#  9. Use nested loops with an array of numbers to compute a new array containing the first two numbers (from the original array) that add up to the number 10. If there are no two numbers that add up to 10, return false.
+#     For example, [2, 5, 3, 1, 0, 7, 11] becomes [3, 7].
+
+# 10. Use a nested loop to convert an array of string arrays into a single string.
+#     For example, [["a", "man"], ["a", "plan"], ["a", "canal"], ["panama"]] becomes "amanaplanacanalpanama".
