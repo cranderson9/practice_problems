e673cc0b086b7a680f8366de3057a22ef3170f8b
cale <caleanderson9@gmail.com>
2020-03-07 12:19:20 -0600

    done with problem 10

--- convert_data.rb
+++ convert_data.rb
@@ -157,27 +157,36 @@
 #  9. Convert an array of hashes into a hash of arrays, using the author as keys and the titles as values.
 #     For example, [{author: "Jeff Smith", title: "Bone"}, {author: "George Orwell", title: "1984"}, {author: "Jeff Smith", title: "RASL"}] becomes {"Jeff Smith" => ["Bone", "RASL"], "George Orwell" => ["1984"]}.
 
-books = [{author: "Jeff Smith", title: "Bone"}, {author: "George Orwell", title: "1984"}, {author: "Jeff Smith", title: "RASL"}] 
-books_hash = {}
+# books = [{author: "Jeff Smith", title: "Bone"}, {author: "George Orwell", title: "1984"}, {author: "Jeff Smith", title: "RASL"}] 
+# books_hash = {}
 #write an outside loop that starts with the first hash
 # make book variable so you can use this variable in all of the hashes
 #make variables author and title so they can be used in all the hashes
 # Then reset the variable books_hash to the author and title
 # loop through adding the other books in the array
-i = 0
+# i = 0
 
-while i < books.length
-  book = books[i]
-  author = books[i][:author]
-  title = books[i][:title]
-  if books_hash[author] == nil
-    books_hash[author] = []
-  end
-  books_hash[author] << title
-  i += 1
-end
+# while i < books.length
+#   book = books[i]
+#   author = books[i][:author]
+#   title = books[i][:title]
+#   if books_hash[author] == nil
+#     books_hash[author] = []
+#   end
+#   books_hash[author] << title
+#   i += 1
+# end
 
-p books_hash
+# p books_hash
 
 # 10. Given a hash, create a new hash that has the keys and values switched.
 #     For example, {"a" => 1, "b" => 2, "c" => 3} becomes {1 => "a", 2 => "b", 3 => "c"}.
+
+# letters = {"a" => 1, "b" => 2, "c" => 3} 
+# hash_switch = {}
+# letters.map do |letter, number|
+#   hash_switch[number] = letter
+# end
+
+# p hash_switch
+
