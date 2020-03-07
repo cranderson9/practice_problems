31ff65323186dbc2d9bd7f4fcf5a1e689e1f3d55
cale <caleanderson9@gmail.com>
2020-03-07 09:06:58 -0600

    done with problem 7

--- test.rb
+++ test.rb
@@ -1,14 +1,1 @@
-# word = "bookkeeper"
-# letter_count = {}
-# i = 0
 
-# while i < word.length
-#   letter = word[i]
-#   if letter_count[letter] == nil
-#     letter_count[letter] = 0
-#   end
-#   letter_count[letter] += 1
-#   i += 1
-# end
-
-# p letter_count
