00ef69c9aaa6baa3d0d0617142a870f9a2f76a40
cale <caleanderson9@gmail.com>
2020-03-15 13:33:14 -0500

    done with loops_within_loops problem 4

--- loop_within_loop.rb
+++ loop_within_loop.rb
@@ -6,19 +6,19 @@
 # loop through that array
 # write an outside loop that loops through all of the other arrays
 
-# numbers = [[1, 3], [8, 9], [2, 16]]
-# numbers_flattened = []
-# i1 = 0
-# while i1 < numbers.length 
-#   i2 = 0
-#   while i2 < numbers[i1].length
-#     numbers_flattened << numbers[i1][i2]
-#     i2 +=1
-#   end
-#   i1 +=1
-# end
+numbers = [[1, 3], [8, 9], [2, 16]]
+numbers_flattened = []
+i1 = 0
+while i1 < numbers.length 
+  i2 = 0
+  while i2 < numbers[i1].length
+    numbers_flattened << numbers[i1][i2]
+    i2 +=1
+  end
+  i1 +=1
+end
 
-# p numbers_flattened
+p numbers_flattened
 
 #  2. Use a nested loop with two arrays of strings to create a new array of strings with each string combined.
 #     For example, ["a", "b", "c"] and ["d", "e", "f", "g"] becomes ["ad", "ae", "af", "ag", "bd", "be", "bf", "bg", "cd", "ce", "cf", "cg"].
@@ -28,20 +28,19 @@
 # write a loop that grabs the first value of the first array and then loops through the second array combining them together and shoveling them into the new array
 #write an outside loop that moves through all of the strings in the first array
 
-# letters1 = ["a", "b", "c"]
-# letters2 = ["d", "e", "f", "g"]
-# combined_letters = []
-# i1 = 0
-# while i1 < letters1.length
-#   i2 = 0
-#   while i2 < letters2.length
-#     combined_letters << letters1[i1] + letters2[i2]
-#     i2 += 1
-#   end
-#   i1 += 1
-# end
-
-# p combined_letters
+letters1 = ["a", "b", "c"]
+letters2 = ["d", "e", "f", "g"]
+combined_letters = []
+i1 = 0
+while i1 < letters1.length
+  i2 = 0
+  while i2 < letters2.length
+    combined_letters << letters1[i1] + letters2[i2]
+    i2 += 1
+  end
+  i1 += 1
+end
+p combined_letters
 
 #  3. Use a nested loop with one array of strings to create a new array that contains every combination of each string with every other string in the array.
 #     For example, ["a", "b", "c", "d"] becomes ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"].
@@ -67,6 +66,28 @@ p letter_combos
 
 #  4. Use a nested loop to find the largest product of any two different numbers within a given array.
 #     For example, [5, -2, 1, -9, -7, 2, 6] becomes 63.
+
+# make a new array and set the value to 0
+# make an outside loop that multiples value 1 by value 2
+# make an inside loop that has an if statment for value 1 and 2 that they can't be equal
+
+numbers = [5, -2, 1, -9, -7, 2, 6]
+max_product = 0
+i1 = 0
+while i1 < numbers.length
+  i2 = 0
+  while i2 < numbers.length
+    if i1 != i2
+      sum = numbers[i1] * numbers[i2]
+      if sum > max_product
+        max_product = sum
+      end
+    end
+    i2 += 1
+  end
+  i1 += 1
+end
+p max_product
 
 #  5. Use a nested loop to compute the sum of all the numbers in an array of number pairs.
 #     For example, [[1, 3], [8, 9], [2, 16]] becomes 39.
